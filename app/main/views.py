048cfed8bd596b59afc60e44b39991e8dbdeef58
muce <imuce9@gmail.com>
2015-03-02 19:50:59 +0000

    first commit

---
+++ app/main/views.py
@@ -0,0 +1,35 @@
+from flask import render_template
+from flask.ext.login import login_required, current_user
+from . import main
+from forms import EditProfileForm
+from ..models import User
+
+
+@main.route('/')
+def index():
+    return render_template('index.html')
+
+@main.route('/user/<username>')
+def user(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        abort(404)
+    return render_template('user.html', user=user)
+
+@main.route('/edit-profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.name = form.name.data
+        current_user.location = form.location.data
+        current_user.about_me = form.about_me.data
+        db.session.add(user)
+        flash('Your profile has been updated.')
+        return redirect(url_for('.user', username=current_user.username))
+    form.name.data = current_user.name
+    form.location.data = current_user.location
+    form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', form=form)
+
+
